5f3217c97ef40598e59439a9af3b161a19c4e718
Igor-Korniichuk <kiv160780@gmail.com>
2022-05-10 15:01:56 +0300

    Промежуточный hw_8-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+
+const galleryEl = document.querySelector('.gallery');
+const itemsGalleryMarkup = createItemGalleryMurkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', itemsGalleryMarkup);
+console.log(galleryEl)
+
+console.log(createItemGalleryMurkup(galleryItems));
+
+function createItemGalleryMurkup(galleryItems) {
+        return galleryItems.map(({ preview, original, description }) =>        
+        (`<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>`)).join('');      
+};
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    fadeSpeed: 250,
+    overlayOpacity: 0.8,
+    captionsData: "alt",
+});
+    
