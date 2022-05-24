3630218081c526a9526d20eb2c1cd4a0e14a292f
Igor-Korniichuk <kiv160780@gmail.com>
2022-05-24 11:30:57 +0300

    Выполняет ДЗ №8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const FORM_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const dataForm = {};
+
+onFormShow();
+
+function onInputForm (event) {
+    dataForm[event.target.name] = event.target.value;
+    localStorage.setItem(FORM_KEY, JSON.stringify(dataForm))
+}
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(FORM_KEY)));    
+    event.currentTarget.reset();
+    localStorage.removeItem(FORM_KEY);
+}
+
+function onFormShow() {  
+  if (localStorage.getItem(FORM_KEY)) {
+    const data = JSON.parse(localStorage.getItem(FORM_KEY));
+    form.elements.email.value = data.email
+    form.elements.message.value = data.message
+  }
+}
